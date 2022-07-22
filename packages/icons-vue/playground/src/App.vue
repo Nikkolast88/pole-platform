679c1ce9d7e9a395d2e8940178abd5ce4af4249e
nikkolast88 <nikkolast88@gmail.com>
2022-07-22 15:44:51 +0800

    feat: svg图标在线预览

---
+++ packages/icons-vue/playground/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { icons } from '@pole-platform/icons-vue/global';
+</script>
+
+<template>
+  <component :is="Icon" v-for="(Icon, key) in icons" :key="key" class="icon" />
+  <hr />
+  <component
+    :is="`PpIcon${key}`"
+    v-for="key in Object.keys(icons)"
+    :key="key"
+    class="icon"
+  />
+</template>
+
+<style scoped>
+.icon {
+  height: 48px;
+  color: #409eff;
+}
+</style>
---
+++ packages/icons-vue/playground/src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue';
+import PpIcons from '@pole-platform/icons-vue/global';
+import App from './App.vue';
+
+createApp(App).use(PpIcons).mount('#app');
